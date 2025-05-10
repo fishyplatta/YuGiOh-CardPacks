1fc430a5762a245610b83126980999c798522c57
LostShoreCreative <internerd005@gmail.com>
2025-05-10 14:38:06 -0600

    Animations pretty much done. More cards added

--- scripts/pack-opening.js
+++ scripts/pack-opening.js
@@ -1,19 +1,35 @@
 const plate = document.getElementById("card-section");
 const pack = document.getElementById("pack");
-const container = document.getElementById("card-container")
-const pile = document.getElementById("pile-img")
+const container = document.getElementById("card-container");
+const pile = document.getElementById("pile");
+const pile_img = document.getElementById("pile-img");
+
+//cards
+const display_card = document.getElementById("display-card");
+const moving_card = document.getElementById("moving-card");
+const moving_img = document.getElementById("moving-img");
+const card_back = document.getElementById("card-back");
+const card_front = document.getElementById("card-front");
 
 var files = 
 [
-    "chadius.jpg",
-    "channel-deletion.jpg",
-    "gtlw.jpg",
-    "mimikyuchris.jpg", 
-    "russian-snow.jpg", 
-    "shhark.jpg", 
-    "sloth.jpg", 
-    "yy-of-the-undercity.jpg",
-    "yy-the-wildfyre.jpg"
+  "carrit.jpg",
+  "chadius.jpg",
+  "channel-deletion.jpg",
+  "g-byakko.jpg",
+  "gtlw.jpg",
+  "last-laugh.jpg",
+  "mimikyuchris.jpg", 
+  "russian-snow.jpg",
+  "scally-wagon.jpg",
+  "scar-apartheid.jpg", 
+  "shhark.jpg", 
+  "skelly-pup.jpg",
+  "sloth.jpg",
+  "teal-magician", 
+  "yenfestation.jpg",
+  "yy-of-the-undercity.jpg",
+  "yy-the-wildfyre.jpg"
 ];
 
 var remaining_cards = 0;
@@ -30,23 +46,64 @@ function RandCard()
 
 function OpenPack()
 {
-    var width = 650;
-    clearInterval(id);
-    id = setInterval(frame, 1);
-    function frame() {
-      if (width == 1400) {
-        clearInterval(id);
-        pack.style.display = "none";
-        container.style.display = "block"
-      } else {
-        width+=5;
-        plate.style.width = width + "px";
-        pack.style.marginLeft = "40.5px";
-      }
+  var width = 650;
+  clearInterval(id);
+  id = setInterval(frame, 1);
+  function frame() 
+  {
+    if (width == 1400) 
+    {
+      clearInterval(id);
+      pack.style.display = "none";
+      container.style.display = "block";
+      pile.style.display = "block";
+    } 
+    else 
+    {
+      width+=5;
+      plate.style.width = width + "px";
+      pack.style.marginLeft = "40.5px";
     }
+  }
 }
 
 function FlipCard()
 {
+  container.style.transitionDuration = "0.5s";
   container.classList.toggle("is-flipped")
+  RandCard();
+  display_card.src = selected_card;
+  moving_img.src = selected_card;
+}
+
+function MoveCard()
+{
+  card_back.style.display = "none";
+  container.style.transitionDuration = "0s";
+  container.classList.toggle("is-flipped")
+  moving_card.style.display = "block";
+  MoveAnimation();
+}
+
+function MoveAnimation()
+{
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 1);
+  function frame() 
+  {
+    if (pos == 679) 
+    {
+      clearInterval(id);
+      pile_img.src = selected_card;
+      moving_card.style.display = "none";
+      moving_card.style.left = 0+"px";
+      card_back.style.display = "block";
+    } 
+    else 
+    {
+      pos+=7;
+      moving_card.style.left = pos + "px";
+    }
+  }
 }
